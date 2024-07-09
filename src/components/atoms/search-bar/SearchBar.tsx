ae148bea6a234837eb26e3390908337b837930d1
Sohan <svaigankar2000@gmail.com>
2024-07-09 15:44:20 +0530

    add: search functionality for transaction data

--- src/components/atoms/search-bar/SearchBar.tsx
+++ src/components/atoms/search-bar/SearchBar.tsx
@@ -1,19 +1,57 @@
 // icons
-import { PiCommand, PiMagnifyingGlass } from "react-icons/pi";
+import { useState } from "react";
+// icons
+import { PiCommand, PiMagnifyingGlass, PiXBold } from "react-icons/pi";
 
-const SearchBar = () => {
+type SearchBarProps = {
+  extraClassName?: string;
+  handleChange?: (arg: string) => void;
+};
+
+const SearchBar = (props: SearchBarProps) => {
+  const { extraClassName, handleChange } = props;
+
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const handleOnChange = (e) => {
+    e.stopPropagation();
+    setSearchQuery(e.target.value);
+    handleChange && handleChange(e.target.value);
+  };
+
+  const handleReset = () => {
+    setSearchQuery("");
+    handleChange && handleChange("");
+  };
+
   return (
-    <form className="relative rounded-md overflow-hidden flex  items-center dark:text-gray-400">
+    <div
+      className={`relative rounded-md overflow-hidden flex  items-center dark:text-gray-400 ${extraClassName}`}
+    >
       <PiMagnifyingGlass className="absolute left-2" fontSize={"1rem"} />
       <input
         type="text"
         placeholder="Search"
+        value={searchQuery}
         className="pl-7  py-1 text-sm rounded-md w-full focus:outline-none"
+        onChange={handleOnChange}
       />
-      <span className="absolute right-2 flex items-center text-sm">
-        <PiCommand fontSize={"1rem"} />/
+      <span
+        className="absolute right-2 flex items-center text-sm"
+        onClick={handleReset}
+      >
+        {searchQuery ? (
+          <PiXBold
+            fontSize={"1rem"}
+            className="hover:cursor-pointer hover:text-red-500"
+          />
+        ) : (
+          <>
+            <PiCommand fontSize={"1rem"} />/
+          </>
+        )}
       </span>
-    </form>
+    </div>
   );
 };
 
--- src/components/organisms/transactions-section/TransactionsSection.tsx
+++ src/components/organisms/transactions-section/TransactionsSection.tsx
@@ -1,4 +1,6 @@
+import { useMemo, useState } from "react";
 // components
+import SearchBar from "../../atoms/search-bar/SearchBar";
 import PaginatedTable from "../../molecules/paginated-table/PaginatedTable";
 // constants, helper fn
 import { MOCK_TRANSACTIONS_DATA } from "../../../utils/constants";
@@ -16,12 +18,38 @@ const TransactionsSection = () => {
     { key: "order_id", header: "Order ID", sortable: false },
     { key: "amount", header: "Amount (in $)" },
   ];
+
+  const [query, setQuery] = useState<string>("");
+
   const transactionData: Transaction[] = MOCK_TRANSACTIONS_DATA;
 
+  const handleSearch = (value: string) => {
+    setQuery(value);
+  };
+
+  // filtering out transaction data based on search query
+  const filteredTransactionData = useMemo(() => {
+    const filteredList = transactionData.filter(
+      (item) =>
+        String(item.id).includes(query) ||
+        item.name.toLowerCase().includes(query) ||
+        item.order_id.toLowerCase().includes(query) ||
+        String(item.amount).includes(query)
+    );
+    return filteredList;
+  }, [query, transactionData]);
+
   return (
-    <div className="container mx-auto mt-8">
+    <div className="container flex flex-col gap-6 mx-auto mt-8 lg:mt-0">
+      <div className="flex flex-col gap-6 xl:flex-row justify-between items-center w-full">
+        <div>Recent Transactions</div>
+        <SearchBar
+          extraClassName="max-w-[600px] w-full"
+          handleChange={handleSearch}
+        />
+      </div>
       <PaginatedTable
-        data={transactionData}
+        data={filteredTransactionData}
         columns={transactionTableHeaders}
         itemsPerPage={10}
       />
